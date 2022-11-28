2a676c8f48b172f4be766cd150e40df2df1d6fd4
Faiaz <faiazarrafi@gmail.com>
2022-11-28 11:24:31 +0600

    myproducts deletion added

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function run() {
             res.send(bookings);
         })
 
-        app.delete('/myproducts:id', async (req, res) => {
+        app.delete('/myproducts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await myProductsCollection.deleteOne(query);
